f87e68fe81ed45ff28da2d44dcdcae3df2e8a2b6
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2026-08-27 16:25:12 +0530

    feat: initial commit

---
+++ ai-service/app/services/llm_service.py
@@ -0,0 +1,6 @@
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_xai import ChatXAI
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+
