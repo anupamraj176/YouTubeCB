cb678370b2b33aad9d8a8e3eaf3226812aec98dd
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2026-08-27 16:42:38 +0530

    feat: add llm_service to handle YouTube transcript ingestion, splitting, and vector indexing

--- ai-service/app/services/llm_service.py
+++ ai-service/app/services/llm_service.py
@@ -1,6 +1,33 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_xai import ChatXAI
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
+
+#Step 1a: Indexing
+video_id = "LPZh9B0jkQs"  # only the ID, not full URL
+
+try:
+    # If you don’t care which language, this returns the “best” one
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"])
+
+    # Flatten it to plain text
+    transcript = " ".join(chunk["text"] for chunk in transcript_list)
+    print(transcript)
+
+except TranscriptsDisabled:
+    print("No captions available for this video.")
+
+#Step 1b : Text Splitting
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = splitter.create_documents([transcript])
+
+#Step 1c and 1d : Text Embedding and Vector Store
+# Groq doesn't have an embeddings API, so we use a free HuggingFace model locally
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+vector_store = FAISS.from_documents(chunks, embeddings)
+print("Vector Store Created Successfully")
+
 
